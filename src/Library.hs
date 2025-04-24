3ce312d8363a12c5e9d5be84140198909e987d44
Enzo Bonafine <ebonafine@frba.utn.edu.ar>
2025-04-24 12:41:08 -0300

    Inicio de TP2

--- src/Library.hs
+++ src/Library.hs
@@ -3,3 +3,35 @@ import PdePreludat
 
 doble :: Number -> Number
 doble numero = numero + numero
+
+data CartasMagicas = UnaCarta{
+    nivel :: Number,
+    palo :: Number,
+    estadoDeActividad :: Bool
+}
+
+data Fuerzas = UnaPersona{
+    nombre :: String,
+    armas :: Number,
+    tieneEspecial :: Bool
+}
+
+dalia :: Fuerzas
+dalia = UnaPersona{nombre="Dalia Centenaria", armas=5, tieneEspecial=True}
+
+gato :: Fuerzas
+gato = UnaPersona{nombre="Gato de Cheshire", armas=3, tieneEspecial=False}
+
+
+
+energiaBaseDalia :: Number
+energiaBaseDalia = 50;
+
+energiaTotalDalia :: Fuerzas -> Number
+energiaTotalDalia energiaDalia =
+    |   tieneEspecial dalia = (armas Dalia)*5
+    |
+
+
+--energiaTotalReina :: Fuerzas -> Fuerzas -> Number
+--energiaTotalReina energia = 0;
